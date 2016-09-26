35cd5616073472e60fcbf3c2e8ecbf6ffcec1ee1
Ambyr La Violette <alaviolette89@gmail.com>
2016-09-26 07:46:33 -0400

    removed block

--- src/vending/VendingMachineApp.java
+++ src/vending/VendingMachineApp.java
@@ -8,31 +8,7 @@ import java.util.Scanner;
 
 public class VendingMachineApp {
 
-	
-	// As a customer
-	// I want to have my money returned
-	// So that I can change my mind about buying stuff from the vending machine
-	// When the return coins button is pressed,
-	// the money the customer has placed in the machine is
-	// returned and the display shows INSERT COIN.
-	// Sold Out
-	// As a customer
-	// I want to be told when the item I have selected is not available
-	// So that I can select another item
-	// When the item selected by the customer is out of stock,
-	// the machine displays SOLD OUT. If the display is checked again,
-	// it will display the amount of money remaining in the machine or INSERT
-	// COIN if
-	// there is no money in the machine.
-	// Exact Change Only
-	// As a customer
-	// I want to be told when exact change is required
-	// So that I can determine if I can buy something with the money I have
-	// before inserting it
 
-	// When the machine is not able to make change with the money in the
-	// machine for any of the items that it sells, it will display EXACT
-	// CHANGE ONLY instead of INSERT COIN.
 	public static void main(String[] args) {
 		ArrayList<Inventory> list = new ArrayList<Inventory>();
 		DecimalFormat df = new DecimalFormat("#0.00");
